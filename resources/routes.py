8a5bd6d5cd15029ca52cd550a329654a3830eeac
Muhammad Farooq <43150822+mfarooq95@users.noreply.github.com>
2022-06-20 17:22:46 -0400

    Update routes.py
    Updated routes.py's comment to reflect the focus on the correct line of code after line numbers changed with the addition of more routes being instantiated

--- resources/routes.py
+++ resources/routes.py
@@ -3,6 +3,6 @@ from .auth import SignupApi, LoginApi
 
 def initialize_routes(api):
     api.add_resource(OneHandedWeaponsAPI, '/api/onehandedweapons')
-    api.add_resource(OneHandedWeaponAPI, '/api/onehandedweapons/<id>') # Note the difference in resources passed and the endpoints in line 7 & 8
+    api.add_resource(OneHandedWeaponAPI, '/api/onehandedweapons/<id>') # Note the difference in resources passed and the endpoints in line 5 & 6
     api.add_resource(SignupApi, '/api/auth/signup')
     api.add_resource(LoginApi, '/api/auth/login')
